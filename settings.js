e9c46e40e0fff9af9f9aede3351ff92b038e4723
Bernardo F. Martínez Meave <50682523+behrmart@users.noreply.github.com>
2025-02-20 16:35:00 +0000

    Course Commit

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "3f7179c552c03728fe2253a96fb6c079",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "cuautitlan izcalli,mx",
 };
